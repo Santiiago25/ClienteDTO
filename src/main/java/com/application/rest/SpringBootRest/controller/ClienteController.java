608c32a0e439a95ed0c562e966c39fa6b80109aa
Santiago Tokora <kevintocora_3d@hotmail.com>
2024-01-02 15:42:53 -0500

    Actualización del ResponseEntity en el metodo Delete, ahora se puso un ResponseEntity un poco mas especifico pero se debe de mejorar

--- src/main/java/com/application/rest/SpringBootRest/controller/ClienteController.java
+++ src/main/java/com/application/rest/SpringBootRest/controller/ClienteController.java
@@ -3,8 +3,13 @@ package com.application.rest.SpringBootRest.controller;
 import com.application.rest.SpringBootRest.model.entity.Cliente;
 import com.application.rest.SpringBootRest.service.ICliente;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -25,10 +30,19 @@ public class ClienteController {
     }
 
     @DeleteMapping("cliente/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id){
-        Cliente clienteDelete = clienteService.findById(id);
-        clienteService.delete(clienteDelete);
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        //estructura basica para el ResponseEntity
+        Map<String, Object> response = new HashMap<>();
+        try{
+            Cliente clienteDelete = clienteService.findById(id);
+            clienteService.delete(clienteDelete);
+            return new ResponseEntity<>(clienteDelete, HttpStatus.NO_CONTENT);
+        }catch (DataAccessException exDt){ //en caso que no encuentre el id que se va a eliminar
+            //vamos a enviar un mapa de errores
+            response.put("mensaje: ", exDt.getMessage()); //enviamos la exception
+            response.put("cliente: ", null);
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR); //error en la base de datos porque no encuentra el id
+        }
     }
 
     @GetMapping("cliente/{id}")
