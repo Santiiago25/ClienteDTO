a8642b7eeff4b44e2dde04b470dd0668dae95bc5
Santiago Tokora <kevintocora_3d@hotmail.com>
2024-01-02 15:51:50 -0500

    Modificación de Cliente a ClienteDto en el interface ICliente

--- src/main/java/com/application/rest/SpringBootRest/service/ICliente.java
+++ src/main/java/com/application/rest/SpringBootRest/service/ICliente.java
@@ -1,13 +1,14 @@
 package com.application.rest.SpringBootRest.service;
 
+import com.application.rest.SpringBootRest.model.dto.ClienteDto;
 import com.application.rest.SpringBootRest.model.entity.Cliente;
 
 public interface ICliente {
 
-    Cliente save(Cliente cliente); //recibe como dato cliente y lo guarda (POST, PUT)
+    ClienteDto save(ClienteDto cliente); //recibe como dato cliente y lo guarda (POST, PUT)
 
-    Cliente findById(Long id); //busca por id (GET)
+    ClienteDto findById(Long id); //busca por id (GET)
 
-    void delete(Cliente cliente); //eliminar el cliente (DELETE)
+    void delete(ClienteDto cliente); //eliminar el cliente (DELETE)
 
 }
