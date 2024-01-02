46cbc4cb4c83d84070068c318cb09b1de28c6e30
Santiago Tokora <kevintocora_3d@hotmail.com>
2024-01-01 20:37:48 -0500

    Se añade la notacion @Transactional nos permite visualizar ciertas excepciones por defecto sin necesidad de programarlas

--- src/main/java/com/application/rest/SpringBootRest/service/impl/ClienteImpl.java
+++ src/main/java/com/application/rest/SpringBootRest/service/impl/ClienteImpl.java
@@ -5,6 +5,7 @@ import com.application.rest.SpringBootRest.model.entity.Cliente;
 import com.application.rest.SpringBootRest.service.ICliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClienteImpl implements ICliente {
@@ -12,17 +13,20 @@ public class ClienteImpl implements ICliente {
     @Autowired //inyeccion de dependencia, la otra forma es creando el constructor
     private ClienteDao clienteDao;
 
+    @Transactional
     @Override
     public Cliente save(Cliente cliente) {
         return clienteDao.save(cliente);
     }
 
+    @Transactional(readOnly = true) //especifica que es solo una consulta
     @Override
     public Cliente findById(Long id) {
         //orElse --> si el objeto no existe lo marca como null
         return clienteDao.findById(id).orElse(null);
     }
 
+    @Transactional
     @Override
     public void delete(Cliente cliente) {
         clienteDao.delete(cliente);
