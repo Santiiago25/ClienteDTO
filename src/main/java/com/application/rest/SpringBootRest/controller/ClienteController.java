f5c0002d2882a685f466866d5d33e1b07d6a5190
Santiago Tokora <kevintocora_3d@hotmail.com>
2024-01-02 15:28:08 -0500

    Se define el ResponseStatus en los metodos POST, PUT, DELETE y GET, estos se definen por defecto

--- src/main/java/com/application/rest/SpringBootRest/controller/ClienteController.java
+++ src/main/java/com/application/rest/SpringBootRest/controller/ClienteController.java
@@ -3,6 +3,7 @@ package com.application.rest.SpringBootRest.controller;
 import com.application.rest.SpringBootRest.model.entity.Cliente;
 import com.application.rest.SpringBootRest.service.ICliente;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,22 +13,26 @@ public class ClienteController {
     private ICliente clienteService; //se llama al servicio
 
     @PostMapping("cliente")
+    @ResponseStatus(HttpStatus.CREATED)
     public Cliente create(@RequestBody Cliente cliente){
         return clienteService.save(cliente);
     }
 
     @PutMapping("cliente")
+    @ResponseStatus(HttpStatus.CREATED)
     public Cliente update(@RequestBody Cliente cliente){
         return clienteService.save(cliente);
     }
 
     @DeleteMapping("cliente/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id){
         Cliente clienteDelete = clienteService.findById(id);
         clienteService.delete(clienteDelete);
     }
 
     @GetMapping("cliente/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Cliente showById(@PathVariable Long id){
         return clienteService.findById(id);
     }
